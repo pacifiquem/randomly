af85eb8dd17d57e6b885e5c16e8883d67b8db1c4
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-12-06 21:57:03 +0200

    feat: proj readme setted up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import Head from 'next/head'
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className=' w-screen h-screen flex justify-center items-center bg-black'>
+      <Head>
+        <title>Randomly - Home</title>
+        <link rel="icon" href="D:/Documents/projects/selfpro/randomly/public/favicon.ico" />
+      </Head>
       <h1 className=' text-2xl text-white'>Initialization</h1>
     </div>
   )
