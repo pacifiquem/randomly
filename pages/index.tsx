c4902ba56c6bc953ae9be6ee14828ab7067dff60
pacifiquem <pacifiquemurangwa001@gmail.com>
2023-01-23 09:00:35 +0200

    init: project initialization
    Co-Author: ndzhwr <ndizihiweregis06@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import Head from 'next/head'
+import Head from 'nextcd ../head';
 
 export default function Home() {
   return (
     <div className=' w-screen h-screen flex justify-center items-center bg-black'>
       <Head>
         <title>Randomly - Home</title>
-        <link rel="icon" href="D:/Documents/projects/selfpro/randomly/public/favicon.ico" />
+        <link rel="icon" href="favicon.ico" />
       </Head>
       <h1 className=' text-2xl text-white'>Initialization</h1>
     </div>
